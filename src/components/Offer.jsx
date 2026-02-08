7626e0049d92cc22e487cc38fefd905cbf119359
Ben-plt <benjamin@entrepreneuraligne.fr>
2026-02-08 18:54:16 +0100

    fix: use scrub instead of toggleActions for benefits animation

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -117,12 +117,12 @@ const Offer = () => {
             { opacity: 0, y: 30 },
             {
                 opacity: 1, y: 0,
-                duration: 0.8,
                 ease: 'power2.out',
                 scrollTrigger: {
                     trigger: benefits,
-                    start: 'top 85%',
-                    toggleActions: 'play none none none'
+                    start: 'top 90%',
+                    end: 'top 60%',
+                    scrub: 0.5
                 }
             }
         );
